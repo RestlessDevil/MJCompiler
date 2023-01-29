e4657c3cfe3d02a634323e0ac8afae84473269cf
Vasilije Rajovic <vasilije@jarvis.asw.local>
2023-01-29 20:26:58 +0100

    Sovled problem with getting values

--- src/rs/ac/bg/etf/pp1/RuleVisitor.java
+++ src/rs/ac/bg/etf/pp1/RuleVisitor.java
@@ -3,12 +3,17 @@ package rs.ac.bg.etf.pp1;
 import org.apache.log4j.Logger;
 
 import rs.ac.bg.etf.pp1.ast.IntAssignment;
+import rs.ac.bg.etf.pp1.ast.Program;
 import rs.ac.bg.etf.pp1.ast.VisitorAdaptor;
 
 public class RuleVisitor extends VisitorAdaptor {
 	Logger log = Logger.getLogger(getClass());
 
+	public void visit(Program program) {
+		System.out.println("IME PROGRAMA: " + program.getName());
+	}
+
 	public void visit(IntAssignment intAssignment) {
-		System.out.println("Dodela celobrojne vrednosti " + intAssignment);
+		System.out.println("DODELA CELOBROJNE VRIJEDNOSTI: " + intAssignment.getValue() + " PROMJENLJIVOJ " + intAssignment.getVarName());
 	}
 }
