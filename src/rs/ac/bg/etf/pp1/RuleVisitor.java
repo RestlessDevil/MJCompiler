39903fc85d53364b19b3e50b7bdc98d299ec1076
Vasilije Rajovic <vasilije@scout.scout>
2023-01-31 17:04:58 +0100

    Resena kaskadna dodela

--- src/rs/ac/bg/etf/pp1/RuleVisitor.java
+++ src/rs/ac/bg/etf/pp1/RuleVisitor.java
@@ -1,8 +1,15 @@
 package rs.ac.bg.etf.pp1;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.apache.log4j.Logger;
 
+import rs.ac.bg.etf.pp1.ast.Assignment;
+import rs.ac.bg.etf.pp1.ast.CascadingSymbols;
+import rs.ac.bg.etf.pp1.ast.MultipleSymbolAssignment;
 import rs.ac.bg.etf.pp1.ast.Program;
+import rs.ac.bg.etf.pp1.ast.SingleSymbolAssignment;
 import rs.ac.bg.etf.pp1.ast.VisitorAdaptor;
 
 public class RuleVisitor extends VisitorAdaptor {
@@ -12,4 +19,24 @@ public class RuleVisitor extends VisitorAdaptor {
 		System.out.println("IME PROGRAMA: " + program.getName());
 	}
 
+	private static List<String> getAssignedSymbolList(CascadingSymbols cs) {
+		List<String> assignedSymbols;
+		if (cs instanceof MultipleSymbolAssignment) {
+			assignedSymbols = getAssignedSymbolList(((MultipleSymbolAssignment) cs).getCascadingSymbols());
+			assignedSymbols.add(((MultipleSymbolAssignment) cs).getSymbolName());
+		} else {
+			assignedSymbols = new LinkedList<String>();
+			assignedSymbols.add(((SingleSymbolAssignment) cs).getSymbolName());
+		}
+		return assignedSymbols;
+	}
+
+	public void visit(Assignment assignment) {
+		CascadingSymbols cs = assignment.getCascadingSymbols();
+		List<String> assignedSymbols = getAssignedSymbolList(cs);
+		System.out.println("Cascade assignment detected");
+		for (String as : assignedSymbols) {
+			System.out.println(as + " = " + assignment.getExpression());
+		}
+	}
 }
